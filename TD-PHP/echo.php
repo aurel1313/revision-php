75bad836025aab90419da1b7139f3da629713c31
aurel13 <aurelienfabre439@gmail.com>
2021-03-29 14:50:49 +0200

    revisions

---
+++ TD-PHP/echo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+        <title>mon premier php</title>
+        <meta charset ="utf-8">
+    </head>
+    <body>
+       Voici le resultat du script php:
+        <?php
+            $texte = "hello world!";
+            echo $texte;
+        ?>
+        
+        
+    </body>
+
+
+</html>
